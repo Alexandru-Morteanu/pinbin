49545cd4d9b6f79ffbacee9464ffe2934b31a62d
Alexandru-Morteanu <clanzigzager@gmail.com>
2024-07-29 15:18:43 +0300

    ok now

--- digital/index.js
+++ digital/index.js
@@ -33,14 +33,8 @@ app.use("/api", retriveRouter);
 app.use("/api", verifyToken);
 
 // Load SSL certificate and key
-const privateKey = fs.readFileSync(
-  "/Users/morteanualexandru/Desktop/pinbin/digital/cert/key.pem",
-  "utf8"
-);
-const certificate = fs.readFileSync(
-  "/Users/morteanualexandru/Desktop/pinbin/digital/cert/cert.pem",
-  "utf8"
-);
+const privateKey = fs.readFileSync("/cert/key.pem", "utf8");
+const certificate = fs.readFileSync("/cert/cert.pem", "utf8");
 
 // Create HTTPS server
 const server = https.createServer(
